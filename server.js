ff0940c78d7ff47f23ca6ffb2875fa9bbf66116f
mawie81 <mawieh@googlemail.com>
2013-11-14 16:00:59 +0100

    Test

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// Module dependencies.
+var express = require('express'),
+    path = require('path');
+
+var app = express();
+
+// Controllers
+var api = require('./lib/controllers/api');
+
+// Express Configuration
+app.configure(function(){
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.static(path.join(__dirname, '.tmp')));
+  app.use(express.static(path.join(__dirname, 'app')));
+  app.use(express.errorHandler());
+});
+
+app.configure('production', function(){
+  app.use(express.favicon(path.join(__dirname, 'public/favicon.ico')));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+// Routes
+app.get('/api/awesomeThings', api.awesomeThings);
+app.get('/api/test', api.test);
+
+// Start server
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Express server listening on port %d in %s mode', port, app.get('env'));
+});
