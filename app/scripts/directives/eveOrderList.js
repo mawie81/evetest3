65965dab8264a0656dc45a394c93ed467f8638f2
mawie81 <mawieh@googlemail.com>
2013-11-18 21:49:02 +0100

    Test8

--- app/scripts/directives/eveOrderList.js
+++ app/scripts/directives/eveOrderList.js
@@ -3,14 +3,17 @@
 angular.module('evetest3App')
   .directive('eveOrderList', function () {
     return {
-      //templateUrl: 'templates/eveOrderList/eveOrderList.html',
-      template: '<div>Test</div>',
-      restrict: 'E'
-     /* link: function postLink(scope, element, attrs) {
-        element.text('this is the eveOrderList directive');
+      templateUrl: 'templates/eveOrderList/eveOrderList.html',
+      //template: '<div>Test</div>',
+      restrict: 'E',
+      replace: true,
+     /* transclude: true,
+     link: function (scope, element, attrs) {
+        element.append('<p>this is the eveOrderList directive</p>');
+        console.log('eveOrderList');
       }*/
-     /* scope: {
+      scope: {
       	orders: '='
-      })*/
+      }
     };
   });
