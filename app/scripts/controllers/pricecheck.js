93b3362d73bf5582ec4f3148cc2fd7e3543b95d1
mawie81 <mawieh@googlemail.com>
2013-12-02 17:56:54 +0100

    finished PriceCheck

--- app/scripts/controllers/pricecheck.js
+++ app/scripts/controllers/pricecheck.js
@@ -3,13 +3,13 @@
 angular.module('evetest3App')
   .controller('PricecheckCtrl', function ($scope, eveService) {
   	window.scope = $scope;
+    $scope.searchText = '';
   	$scope.selectedItems = [];
   	$scope.selectedItemName = '';
   	$scope.selectedItem = {};
 
   	$scope.chars = [];
-  	$scope.selectedCharName = '';
-  	$scope.selectedCharId = 0;
+  	$scope.selectedCharName = {};
 
   	$scope.selectedStationName = '';
   	$scope.selectedStationID = 0;
@@ -28,7 +28,9 @@ angular.module('evetest3App')
         jitabuy: 0,
         jitasell: 0,
         localbuy: 0,
-        localsell: 0
+        localsell: 0,
+        localMargin: 0,
+        jitaMargin: 0
       });
     	$scope.selectedItem = {};
     	$scope.selectedItemName = '';
@@ -47,7 +49,6 @@ angular.module('evetest3App')
       for (var i = 0; i < $scope.selectedItems.length; i++) 
         typeIDs.push($scope.selectedItems[i].typeID);
      eveService.fetchprices($scope.selectedStationID, typeIDs).success(function(data) {
-      console.log(data);
       for (var i = 0; i < $scope.selectedItems.length; i++) {
         var item = $scope.selectedItems[i];
         for (var j in Object.keys(data)) {
@@ -57,6 +58,8 @@ angular.module('evetest3App')
             item.jitabuy = resp.jitaBuy;
             item.localsell = resp.localSell;
             item.localbuy = resp.localBuy;
+            item.localMargin = getMargin(resp.localSell, resp.localBuy);
+            item.jitaMargin = getMargin(resp.localSell, resp.jitaSell);
           }
         }
       }
@@ -65,13 +68,50 @@ angular.module('evetest3App')
      
     }
 
-    $scope.onSelectChar = function(char) {
-    	$scope.selectedCharId = char.charID;
-    	$scope.selectedCharName = char.charName;
+    $scope.fetchFromChar = function() {
+      var charID = $scope.selectedChar.charID;
+      eveService.getTransactions(charID).success(function(data) {
+        var uniqueTypes = [];
+        var uniqueTypeIDs = [];
+        for (var tan in data) {
+          if (uniqueTypeIDs.indexOf(data[tan].typeID) === -1) {
+            uniqueTypes.push({ 
+              typeID: data[tan].typeID,
+              typeName: data[tan].typeName,
+              jitabuy: 0,
+              jitasell: 0,
+              localbuy: 0,
+              localsell: 0,
+              localMargin: 0,
+              jitaMargin: 0
+            });
+            uniqueTypeIDs.push(data[tan].typeID);
+          }
+        }
+        $scope.selectedItems = uniqueTypes;
+      });
     }
 
-    $scope.getMargin = function(sell, buy) {
+    function getMargin(sell, buy) {
       if (!sell || !buy) return 0;
       return ((sell - buy) / sell) * 100;
     }
+
+    eveService.getCharIDs().success(function(result) {
+      $scope.chars = result;
+    });
+
+    $scope.sort = {
+            column: 'localMarign',
+            descending: true
+        };    
+    $scope.changeSorting = function(column) {
+        var sort = $scope.sort;
+        if (sort.column == column) {
+            sort.descending = !sort.descending;
+        } else {
+            sort.column = column;
+            sort.descending = false;
+        }
+    };
   });
