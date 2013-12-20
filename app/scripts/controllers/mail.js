cd2e26f06c84c1b00036adffe8e18d603a545b5a
mawie81 <mawieh@googlemail.com>
2013-12-20 16:39:50 +0100

    Mails

--- app/scripts/controllers/mail.js
+++ app/scripts/controllers/mail.js
@@ -4,7 +4,27 @@ angular.module('evetest3App')
   .controller('MailCtrl', function ($scope, $routeParams, eveService) {
   	window.scope = $scope;
   	var charID = $routeParams.charId
+
+  	$scope.sort = {
+            column: 'sentDate',
+            descending: true
+        };    
+
   	eveService.getMail(charID).success(function(data) { 
   		$scope.mails = data;
   	});
+
+  	$scope.changeSorting = function(column) {
+        var sort = $scope.sort;
+        if (sort.column == column) {
+            sort.descending = !sort.descending;
+        } else {
+            sort.column = column;
+            sort.descending = false;
+        }
+    };
+
+    $scope.openMail = function(messageID) {
+
+    };
   });
